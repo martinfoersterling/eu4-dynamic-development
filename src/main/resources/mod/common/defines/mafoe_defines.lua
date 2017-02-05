32fde41164d5a3173f26faa609d86ec8d6a7cc01
Martin Försterling <martinleanderfoersterling@gmail.com>
2017-02-05 23:41:04 +0100

    fidgeting with exact bonuses

--- src/main/resources/mod/common/defines/mafoe_defines.lua
+++ src/main/resources/mod/common/defines/mafoe_defines.lua
@@ -8,4 +8,4 @@ NDefines.NDiplomacy.MARCH_BASE_DEVELOPMENT_CAP = 1000
 NDefines.NDiplomacy.ANNEX_DIP_COST_PER_DEVELOPMENT = 2
 NDefines.NCountry.PS_IMPROVE_PROVINCE_BASE = 1000
 
-NDefines.NGraphics.CITY_SPRAWL_AMOUNT = 0.3, 						-- Size of cities, higher gives larger cities
+NDefines.NGraphics.CITY_SPRAWL_AMOUNT = 0.3 						-- Size of cities, higher gives larger cities
