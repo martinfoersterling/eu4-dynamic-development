4c324a2343a5b4f9cd83f56bdb0ab0fdc4413826
Martin Försterling <martinleanderfoersterling@gmail.com>
2017-02-11 21:36:35 +0100

    more terraforming transformations and changed development progress to on_action and hardcore variable action

--- src/main/resources/mod/common/defines/mafoe_defines.lua
+++ src/main/resources/mod/common/defines/mafoe_defines.lua
@@ -3,6 +3,6 @@
 NDefines.NDiplomacy.VASSALIZE_BASE_DEVELOPMENT_CAP = 500
 NDefines.NDiplomacy.MARCH_BASE_DEVELOPMENT_CAP = 500
 NDefines.NDiplomacy.ANNEX_DIP_COST_PER_DEVELOPMENT = 2
-NDefines.NCountry.PS_IMPROVE_PROVINCE_BASE = 1000
+NDefines.NCountry.PS_IMPROVE_PROVINCE_BASE = 10000
 
 NDefines.NGraphics.CITY_SPRAWL_AMOUNT = 0.3 						-- Size of cities, higher gives larger cities
