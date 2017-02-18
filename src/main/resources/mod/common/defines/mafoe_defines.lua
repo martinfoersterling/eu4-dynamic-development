36dce08aee5099c3288b31c5588ab6ed2430ae91
Martin Försterling <martinleanderfoersterling@gmail.com>
2017-02-18 14:38:23 +0100

    so NOR is Norway, not the logical NOR.... you gotta be kidding me

--- src/main/resources/mod/common/defines/mafoe_defines.lua
+++ src/main/resources/mod/common/defines/mafoe_defines.lua
@@ -3,9 +3,6 @@
 NDefines.NDiplomacy.VASSALIZE_BASE_DEVELOPMENT_CAP = 500
 NDefines.NDiplomacy.MARCH_BASE_DEVELOPMENT_CAP = 500
 NDefines.NDiplomacy.ANNEX_DIP_COST_PER_DEVELOPMENT = 2
-NDefines.NCountry.PS_IMPROVE_PROVINCE_BASE = 10000
-
-NDefines.NGraphics.CITY_SPRAWL_AMOUNT = 0.3 						-- Size of cities, higher gives larger cities
 
 NDefines.NCountry.PS_MAKE_PROVINCE_CORE = 0
 NDefines.NCountry.CORE_COLONY = 0
